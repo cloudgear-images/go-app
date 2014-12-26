e4b95c4e7848cf4488805bbd30d8f7ad0434f74f
Georg Kunz <kwd@gmx.ch>
2014-12-26 20:47:13 +0100

    Initial example go app

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"os"
+)
+
+type Info struct{}
+
+func (i *Info) Hostname() string {
+	name, err := os.Hostname()
+	if err != nil {
+		name = ""
+	}
+	return name
+}
+
+func (i *Info) Env() []string {
+	return os.Environ()
+}
+
+func main() {
+	m := martini.Classic()
+	m.Use(render.Renderer(render.Options{Layout: "layout"}))
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", &Info{})
+	})
+	m.Run()
+}
